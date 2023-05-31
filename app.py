3a0f38f827d7ee0436e19adfebb80852cb3db1da
Kevin-C-S-U <k.cohen@uniandes.edu.co>
2023-05-31 01:52:55 -0500

    Actualización a los servicios (Revisar imagenes)

--- app.py
+++ app.py
@@ -27,42 +27,81 @@ def read_item(item_id: int, q: str|None = None):
 @app.get("/get-top-10-courses")
 def top_10_courses():
     query = '''MATCH (user) -[:ns0__tookCourse]->(courseTaken)
-                RETURN courseTaken.ns0__name, SIZE(COLLECT(user)) AS userCount
+                RETURN courseTaken.ns0__name as name, courseTaken.ns0__language as language, courseTaken.ns0__description as description, courseTaken.uri as uri, SIZE(COLLECT(user)) AS userCount
                 ORDER BY userCount DESC LIMIT 10;'''
     result = neo4jRequests.execute_query(query)
-    return result
+    for d in result:
+        del d["userCount"]
+        # name =d["name"]
+        # d["urlImage"] = images.get_image_by_keyword(name)
+    return {"courses":result}
 
-@app.get("/courses_by_related_term")
-def courses_by_related_term():
-    query = '''MATCH (user)-[:ns0__tookCourse]->(courses)
+@app.get("/courses_by_related_term/{username}")
+def courses_by_related_term(username:str):
+    query = f'''MATCH (user)-[:ns0__tookCourse]->(courses)
                 MATCH (courses)-[:ns0__hasKeyTerm]->(keyterm)
                 MATCH (relatedCourse)-[:ns0__hasKeyTerm]->(keyterm)
-                WHERE (user.ns0__personName = "Philip_Vega_Sánchez") and NOT (user)-[:ns0__tookCourse]->(relatedCourse)
-                RETURN DISTINCT relatedCourse, keyterm  LIMIT 10'''
-    result = neo4jRequests.execute_query(query)
-    return result
+                WHERE (user.ns0__personName = "{username}") and NOT (user)-[:ns0__tookCourse]->(relatedCourse)
+                RETURN DISTINCT relatedCourse.ns0__name as name, relatedCourse.ns0__language as language, relatedCourse.ns0__description as description, relatedCourse.uri as uri, keyterm.ns0__hasTerm as KeyTerm'''
+    result = random.sample(neo4jRequests.execute_query(query),15)
+    # for i in result:
+    #     name =i["name"]
+    #     i["urlImage"] = images.get_image_by_keyword(name)
+    return {"courses":result}
 
-@app.get("/favorite-category")
-def favorite_category():
-    query = '''MATCH (user)-[:ns0__tookCourse]->(courseTaken)
+@app.get("/favorite-category/{username}")
+def favorite_category(username:str):
+    query = f'''MATCH (user)-[:ns0__tookCourse]->(courseTaken)
                 MATCH (courseTaken)-[:ns0__courseHas]->(category)
-                WHERE user.ns0__personName = "Carolina_Clark_Clark"
-                RETURN category.ns0__name ,SIZE(COLLECT(courseTaken)) AS courseCount
+                WHERE user.ns0__personName = "{username}"
+                RETURN category.ns0__name AS Category,SIZE(COLLECT(courseTaken)) AS courseCount
                 ORDER BY courseCount DESC
                 LIMIT 10;'''
     result = neo4jRequests.execute_query(query)
-    return result
+    cati = {'categories':[]}
+    for cat in result:
+        query2 = f'''
+            MATCH (category)-[:ns0__hasCourse]->(relatedCourse)
+            WHERE category.ns0__name = "{cat["Category"]}"
+            RETURN  relatedCourse.ns0__name as name, relatedCourse.ns0__language as language, relatedCourse.ns0__description as description, relatedCourse.uri as uri
+        '''
+        exc = neo4jRequests.execute_query(query2)
+        if len(exc)>=10:
+            result2 = random.sample(exc,10)
+        else:
+            result2 = exc
+        cati["categories"].append({"category":cat["Category"], "courses":result2})
+
+        # for i in result2:
+        #     name =i["name"]
+        #     i["urlImage"] = images.get_image_by_keyword(name)
+    return cati
 
 
-@app.get("/courses_by_language")
-def course_by_language():
-    query = '''
+@app.get("/courses_by_language/{username}")
+def course_by_language(username:str):
+    query = f'''
                 MATCH (user)-[:ns0__tookCourse]->(course)
                 MATCH (otherCourse:ns0__Course)
-                WHERE course.ns0__language = otherCourse.ns0__language AND NOT (user)-[:ns0__tookCourse]->(otherCourse) AND user.ns0__personName = "Carolina_Clark_Clark"
+                WHERE course.ns0__language = otherCourse.ns0__language AND NOT (user)-[:ns0__tookCourse]->(otherCourse) AND user.ns0__personName = "{username}"
                 RETURN otherCourse.ns0__name as name, otherCourse.ns0__language as language, otherCourse.ns0__description as description, otherCourse.uri as uri;'''
     result = random.sample(neo4jRequests.execute_query(query),10)
-    for i in result:
-        name =i["name"]
-        i["urlImage"] = images.get_image_by_keyword(name)
+    # for i in result:
+    #     name =i["name"]
+    #     i["urlImage"] = images.get_image_by_keyword("test")
+    return {"courses":result}
+
+@app.get("/courses_partners/{username}")
+def courses_partners(username:str):
+    query = f'''
+                MATCH (user)-[:ns0__tookCourse]->(course)
+                MATCH (otherUser)-[:ns0__tookCourse]->(course)
+                MATCH (otherUser)-[:ns0__tookCourse]->(otherCourse)
+                WHERE user.ns0__personName = "{username}" AND otherUser <> user AND NOT (user)-[:ns0__tookCourse]->(otherCourse)
+                RETURN DISTINCT otherCourse.ns0__name as name, otherCourse.ns0__language as language, otherCourse.ns0__description as description, otherCourse.uri as uri;
+    '''
+    result = random.sample(neo4jRequests.execute_query(query),10)
+    # for i in result:
+    #     name =i["name"]
+    #     i["urlImage"] = images.get_image_by_keyword("test")
     return {"courses":result}
